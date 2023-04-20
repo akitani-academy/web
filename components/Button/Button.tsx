febfae5fb490b04ec488777cf5bac49ee5480c11
2001Y <15167896+2001Y@users.noreply.github.com>
2023-04-20 11:24:05 +0000

    add All

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -9,7 +9,7 @@ export default function Button({ children, href }) {
     return (
         <div className={css_Button.button}>
             <Link href={href}>
-                {children}
+                <div>{children}</div>
                 <div className={css_Button.right}>
                     <svg viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M5.25813 3.51613L8.742 7L5.25813 10.4839" stroke="white" stroke-width="1.16129" stroke-linecap="round" stroke-linejoin="round" />
