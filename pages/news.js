c431dc9f4ad9c5b24d5dfaeb422c438676f6eb97
2001Y <15167896+2001Y@users.noreply.github.com>
2023-04-22 16:46:13 +0000

    add All

--- pages/news.js
+++ pages/news.js
@@ -4,6 +4,7 @@ import Layout from "components/layout";
 import Head from "components/head";
 import Contact from "components/widget/Contact"
 
+import css_global from "styles/global.module.scss";
 import css_contact from "styles/contact.module.scss";
 export default function Page({ wpDATA }) {
   return (
@@ -13,18 +14,20 @@ export default function Page({ wpDATA }) {
         description={wpDATA.summary.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g, '')}
         breadcrumb={[[wpDATA.subTitle, "/news"]]}
       />
-      <div className="thumbnail" >
-        <Image
-          alt={"秋谷光子アカデミィの生徒のの写真"}
-          src={wpDATA.thumbnail}
-          // width={"400"}
-          // height={"300"}
-          layout="fill"
-          objectFit="cover"
-        ></Image>
-      </div>
-      <h1 data-subtitle={wpDATA.subTitle}>{wpDATA.title}</h1>
-      <article className="news" dangerouslySetInnerHTML={{ __html: wpDATA.content }} />
+      <article className={css_global.card}>
+        <div className="thumbnail" >
+          <Image
+            alt={"秋谷光子アカデミィの生徒のの写真"}
+            src={wpDATA.thumbnail}
+            // width={"400"}
+            // height={"300"}
+            layout="fill"
+            objectFit="cover"
+          ></Image>
+        </div>
+        <h1 data-subtitle={wpDATA.subTitle}>{wpDATA.title}</h1>
+        <div className="news" dangerouslySetInnerHTML={{ __html: wpDATA.content }} />
+      </article>
       <aside className={css_contact.contact}>
         <Contact short="true" />
       </aside>
