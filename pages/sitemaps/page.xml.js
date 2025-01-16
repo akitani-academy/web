e8b8f02a6e75939a96bef561253f0a582fb1872c
Yuki <aoyagi.pc.1007@gmail.com>
2025-01-16 12:25:21 +0900

    開発タスクNo.255　対応

--- pages/sitemaps/page.xml.js
+++ pages/sitemaps/page.xml.js
@@ -2,48 +2,48 @@ const _V = require("components/_V");
 import { getPages } from "lib/sitemapGET";
 
 const Sitemap = () => {
-	return null;
+    return null;
 };
 
 export const getServerSideProps = async ({ res }) => {
-	res.setHeader("Content-Type", "text/xml");
-	res.setHeader("Cache-Control", "s-maxage=86400, stale-while-revalidate"); // 24時間のキャッシュ
-	res.write(sitemapXML(await getPages()));
-	res.end();
+    res.setHeader("Content-Type", "text/xml");
+    res.setHeader("Cache-Control", "s-maxage=86400, stale-while-revalidate"); // 24時間のキャッシュ
+    res.write(sitemapXML(await getPages()));
+    res.end();
 
-	return {
-		props: {},
-	};
+    return {
+        props: {},
+    };
 };
 
 export default Sitemap;
 
 function sitemapXML(posts) {
-	let xml = `<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`;
+    let xml = `<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`;
 
-	posts.forEach((post) => {
-		if (post.slug != "course_and_classtype") {
-			if (post.slug == "index") {
-				post.slug = "";
-			} else {
-				post.slug = `/${post.slug}`;
-			}
-			xml += `
+    posts.forEach((post) => {
+        if (post.slug != "course_and_classtype" && post.slug != "privacy") {
+            if (post.slug == "index") {
+                post.slug = "";
+            } else {
+                post.slug = `/${post.slug}`;
+            }
+            xml += `
 			<url>
 				<loc>${_V.meta.baseURL}${post.slug}</loc>
 				<lastmod>${post.dateSitemap}</lastmod>
 			</url>
 			`;
-		}
-	});
+        }
+    });
 
-	xml += `
+    xml += `
 			<url>
 				<loc>${_V.meta.baseURL}/access</loc>
 				<lastmod>2023-04-23</lastmod>
 			</url>
 			`;
 
-	xml += `</urlset>`;
-	return xml;
+    xml += `</urlset>`;
+    return xml;
 }
